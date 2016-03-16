7b7929b189bfc9f56e82d0538fdfdaa7ffca165b
Norton Pengra <npengra317@gmail.com>
2016-03-15 19:32:22 -0700

    Updated to use Postgresql

--- learning_journal/learning_journal/models.py
+++ learning_journal/learning_journal/models.py
@@ -3,7 +3,8 @@ from sqlalchemy import (
     Column,
     Index,
     Integer,
-    Text,
+    Unicode,
+    UnicodeText,
     DateTime,
 )
 from sqlalchemy.ext.declarative import declarative_base
@@ -18,10 +19,10 @@ Base = declarative_base()
 
 
 class Entry(Base):
-    __tablename__ = 'models'
+    __tablename__ = 'entries'
     id = Column(Integer, unique=True, primary_key=True)
-    title = Column(Text(length=128, convert_unicode=True))
-    text = Column(Text(convert_unicode=True))
+    title = Column(Unicode(128), unique=True, nullable=False)
+    text = Column(UnicodeText)
     created = Column(DateTime, default=datetime.datetime.utcnow)
 
 Index('my_index', Entry.title, unique=True, mysql_length=255)
