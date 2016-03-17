57999537f82e408119d244d911d635cd92880d5e
Norton Pengra <npengra317@gmail.com>
2016-03-16 17:51:37 -0700

    Added new patterns

--- learning_journal/learning_journal/__init__.py
+++ learning_journal/learning_journal/__init__.py
@@ -16,6 +16,10 @@ def main(global_config, **settings):
     config = Configurator(settings=settings)
     config.include('pyramid_chameleon')
     config.add_static_view('static', 'static', cache_max_age=3600)
-    config.add_route('home', '/')
+
+    config.add_route('add_entry', '/entry/create')
+    config.add_route('detail', '/entry/{entry_number}')
+    config.add_route('list', '/')
+
     config.scan()
     return config.make_wsgi_app()
