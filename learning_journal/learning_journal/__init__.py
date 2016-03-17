9d211c1e40d100f3129ead09e90279f35e70360c
Norton Pengra <npengra317@gmail.com>
2016-03-16 17:21:27 -0700

    Removed extra whitespace

--- learning_journal/learning_journal/__init__.py
+++ learning_journal/learning_journal/__init__.py
@@ -17,6 +17,5 @@ def main(global_config, **settings):
     config.include('pyramid_chameleon')
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
-    
     config.scan()
     return config.make_wsgi_app()
