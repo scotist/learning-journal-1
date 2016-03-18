b990240c5a66c68eb01a224025534ff7ada1786a
Norton Pengra <npengra317@gmail.com>
2016-03-17 18:24:25 -0700

    Created forms for submitting entries

---
+++ learning_journal/forms.py
@@ -0,0 +1,11 @@
+from wtforms import Form, StringField, TextAreaField, validators
+from wtforms import HiddenField
+
+strip_filter = lambda x: x.strip() if x else None
+
+class EntryCreateForm(Form):
+    title = StringField('title', [validators.Length(min=1, max=255)])
+    text = TextAreaField('text', [validators.Length(min=1)], filters=[strip_filter])
+
+class EntryUpdateForm(EntryCreateForm):
+    id = HiddenField()
