781fe27e6639347d5cdf822f96d92fdb896122a8
Norton Pengra <npengra317@gmail.com>
2016-03-21 18:50:09 -0700

    Added docstrings

--- learning_journal/learning_journal/tests/test_models.py
+++ learning_journal/learning_journal/tests/test_models.py
@@ -11,14 +11,18 @@ def test_create_mymodel_entry(dbtransaction, dummy_request):
     DBSession.flush()
     assert new_model.id is not None
 
+
 def test_create_mymodel_text(dbtransaction, dummy_request):
+    """Test text creation of model."""
     new_model = Entry(title="Norton", text="waffles")
     assert new_model.id is None
     DBSession.add(new_model)
     DBSession.flush()
     assert new_model.text == 'waffles'
 
+
 def test_create_mymodel_title(dbtransaction, dummy_request):
+    """Test title creation of model."""
     new_model = Entry(title="Norton", text="waffles")
     assert new_model.id is None
     DBSession.add(new_model)
@@ -27,6 +31,7 @@ def test_create_mymodel_title(dbtransaction, dummy_request):
 
 
 def test_create_mymodel_created(dbtransaction, dummy_request):
+    """Test "created" creation of model."""
     new_model = Entry(title="Norton", text="waffles")
     assert new_model.id is None
     DBSession.add(new_model)
