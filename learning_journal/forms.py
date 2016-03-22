5e39d6204e0fd89bfafb50d2637ca82e1b6a530d
Norton Pengra <npengra317@gmail.com>
2016-03-21 20:40:14 -0700

    Fixed linting

--- learning_journal/forms.py
+++ learning_journal/forms.py
@@ -3,12 +3,16 @@ from wtforms import HiddenField
 
 strip_filter = lambda x: x.strip() if x else None
 
+
 class EntryCreateForm(Form):
     title = StringField('title', [validators.Length(min=1, max=255)])
-    text = TextAreaField('text', [validators.Length(min=1)], filters=[strip_filter])
+    text = TextAreaField('text', [validators.Length(min=1)],
+                         filters=[strip_filter])
+
 
 class EntryUpdateForm(EntryCreateForm):
     id = HiddenField()
+
 
 class LoginForm(Form):
     username = StringField('username', [validators.Length(min=1, max=16)])
